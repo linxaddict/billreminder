65b7e94a458481de8086bce59af50ece2713b1c7
mprzepiorkowski <mprzepiorkowski@gmail.com>
2017-02-18 21:10:56 +0100

    auth: created auth package and moved all views there

--- billreminder/api/__init__.py
+++ billreminder/api/__init__.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
 from billreminder.api.v1 import *
+from billreminder.api.v1.auth import *
 
 __author__ = 'Marcin Przepiórkowski'
 __email__ = 'mprzepiorkowski@gmail.com'
-
-__all__ = ['v1']
 
 blueprint = Blueprint('api_v1', __name__)
